d06474360d32f5398aa53dc00b42e872a2b57294
Khoa Thai <khoathaiviet@gmail.com>
2017-03-26 19:59:22 +0700

    Init app, eslint, test

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../App';
+
+it('renders welcome message', () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h2>Welcome to React</h2>;
+  expect(wrapper.contains(welcome)).toEqual(true);
+});
